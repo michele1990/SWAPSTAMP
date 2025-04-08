f8bdb512b78650f042338b045776ca5840e21f88
Michele Maestrale <northwestwind@Micheles-MacBook-Air.local>
2025-04-08 21:58:33 +0200

    infra setup

--- infrastructure/dns_validation.tf
+++ infrastructure/dns_validation.tf
@@ -4,11 +4,14 @@ resource "aws_route53_zone" "zone" {
 
 locals {
   unique_validation_options = {
-    for dvo in aws_acm_certificate.prod_app_cert.domain_validation_options : dvo.resource_record_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
+    for key, group in {
+      for dvo in aws_acm_certificate.prod_app_cert.domain_validation_options :
+      dvo.resource_record_name => {
+        name  = dvo.resource_record_name
+        type  = dvo.resource_record_type
+        value = dvo.resource_record_value
+      }...
+    } : key => group[0]
   }
 }
 
@@ -24,5 +27,7 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "prod_app_cert_validation" {
   provider                = aws.us_east_1
   certificate_arn         = aws_acm_certificate.prod_app_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  validation_record_fqdns = [
+    for record in aws_route53_record.cert_validation : record.fqdn
+  ]
 }

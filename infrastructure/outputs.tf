0f8732b3b49fb3ce16035dc7ee0d337f81747063
Michele Maestrale <northwestwind@Micheles-MacBook-Air.local>
2025-04-09 18:15:24 +0200

    setup_6

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,14 +1,19 @@
+output "nameservers" {
+  description = "Nameservers for the Route53 hosted zone. Update your registrar with these values."
+  value       = aws_route53_zone.primary.name_servers
+}
+
+output "s3_bucket_name" {
+  description = "The S3 bucket used for website hosting."
+  value       = aws_s3_bucket.website_bucket.bucket
+}
+
+output "cloudfront_domain" {
+  description = "CloudFront distribution domain name."
+  value       = aws_cloudfront_distribution.website_distribution.domain_name
+}
+
 output "certificate_arn" {
   description = "The ARN of the ACM certificate."
   value       = aws_acm_certificate.certificate.arn
-}
-
-output "certificate_domain_validation_options" {
-  description = "The domain validation options for the certificate."
-  value       = aws_acm_certificate.certificate.domain_validation_options
-}
-
-output "zone_nameservers" {
-  description = "Nameservers for the Route53 hosted zone. Update your registrar with these values."
-  value       = aws_route53_zone.primary.name_servers
 }

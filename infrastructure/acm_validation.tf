efed51be42c6d1ff2db5a09784af76eee1d9b584
Michele Maestrale <northwestwind@Micheles-MacBook-Air.local>
2025-04-09 18:38:40 +0200

    setup_6

--- infrastructure/acm_validation.tf
+++ infrastructure/acm_validation.tf
@@ -1,5 +1,5 @@
 locals {
-  # Convert the set of domain validation options into a list of objects
+  # Convert the set of validation options into a list of objects
   cert_dvos_list = [
     for dvo in aws_acm_certificate.certificate.domain_validation_options : {
       name  = dvo.resource_record_name
@@ -7,17 +7,16 @@ locals {
       value = dvo.resource_record_value
     }
   ]
-
-  # Deduplicate by record name. This prevents duplicate records if ACM returns similar entries.
-  unique_cert_dvos = { for dvo in local.cert_dvos_list : dvo.name => dvo }
+  # Group by record name. If duplicates exist, they are collected as a list.
+  unique_cert_dvos = { for dvo in local.cert_dvos_list : dvo.name => dvo ... }
 }
 
 resource "aws_route53_record" "cert_validation" {
   for_each = local.unique_cert_dvos
   zone_id  = data.aws_route53_zone.primary.zone_id
-  name     = each.value.name
-  type     = each.value.type
-  records  = [each.value.value]
+  name     = each.key
+  type     = each.value[0].type      # Use the first element from the group
+  records  = [ each.value[0].value ]  # Use the first element's value
   ttl      = 60
 }
 

1f9525f41ad588aded82f4ac700e05b12017657b
Michele Maestrale <northwestwind@Micheles-MacBook-Air.local>
2025-04-12 10:40:06 +0200

    setup routing website

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,29 +1,27 @@
 resource "aws_s3_bucket" "website_bucket" {
-  # Il nome include "swapstamp-webapp" per garantire la sua unicità
-  bucket = "swapstamp-webapp-${var.domain_name}"
-  acl    = "public-read"
-  
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-  
+  bucket        = "swapstamp-webapp-${var.domain_name}"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
+resource "aws_s3_bucket_acl" "website_bucket_acl" {
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.website_bucket.id
 
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
-      }
-    ]
-  })
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+locals {
+  # Costruiamo manualmente l'endpoint S3 in modalità website. Notare che la sintassi
+  # è valida per regioni diverse da us-east-1; per us-east-1 l'endpoint potrebbe variare.
+  s3_website_endpoint = "${aws_s3_bucket.website_bucket.id}.s3-website-${var.region}.amazonaws.com"
 }

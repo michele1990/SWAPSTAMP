d59f6258a07d9b4b0d4047b56d208765b9ef96d3
Michele Maestrale <northwestwind@Micheles-Air.station>
2025-04-21 15:43:59 +0200

    fix

--- infrastructure/acm.tf
+++ infrastructure/acm.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
 resource "aws_acm_certificate" "cert" {
   provider                  = aws.us_east_1
   domain_name               = var.domain_name                # swapstamp.com
